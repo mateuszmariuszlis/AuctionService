745048e6d9ffd16d3e8fa7dd8ae938200819ec49
mateuszmariuszlis <mateusz.mariusz.lis@gmail.com>
2017-10-23 12:42:29 +0200

    Added new columns(created_at, updated_at, expires_at, status) to Auction entity

--- Workspace/auction.io/src/AppBundle/Entity/Auction.php
+++ Workspace/auction.io/src/AppBundle/Entity/Auction.php
@@ -12,6 +12,9 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Auction
 {
+    const STATUS_ACTIVE = "active";
+    const STATUS_FINISHED = "finished";
+    const STATUS_CANCELLED = "cancelled";
     /**
      * @var int
      *
@@ -36,13 +39,46 @@ class Auction
     private $description;
 
     /**
-     * @var string
+     * @var float
      *
      * @ORM\Column(name="price", type="decimal", precision=10, scale=2)
      */
     private $price;
 
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="staring_price", type="decimal", precision=10, scale=2)
+     */
+    private $startingPrice;
 
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="expires_at", type="datetime")
+     */
+    private $expiresAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=10)
+     */
+    private $status;
     /**
      * Get id
      *
@@ -123,6 +159,105 @@ class Auction
     public function getPrice()
     {
         return $this->price;
+    }
+
+    /**
+     * @param float $startingPrice
+     *
+     * @return $this
+     */
+    public function setStartingPrice($startingPrice)
+    {
+        $this->startingPrice = $startingPrice;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getStartingPrice()
+    {
+        return $this->startingPrice;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     *
+     * @return $this
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     *
+     * @return $this
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+
+    /**
+     * @param \DateTime $expiresAt
+     *
+     * @return $this
+     */
+    public function setExpiresAt($expiresAt)
+    {
+        $this->expiresAt = $expiresAt;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     *
+     * @return $this
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
     }
 }
 
