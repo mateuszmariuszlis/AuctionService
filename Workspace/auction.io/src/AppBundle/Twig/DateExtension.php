a8b76eb9a3a1b99ca66fb1c6fab150eb13580a8a
mateuszmariuszlis <mateusz.mariusz.lis@gmail.com>
2017-10-27 17:43:30 +0200

    Added intervals in auction details twig

---
+++ Workspace/auction.io/src/AppBundle/Twig/DateExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Twig;
+
+class DateExtension extends \Twig_Extension
+{
+
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter("expireDate", [$this, "expireDate"])
+        ];
+    }
+
+    public function expireDate(\DateTime $expiresAt)
+    {
+        $now = new \DateTime();
+        $interval = $expiresAt->diff($now);
+
+        if ($expiresAt > new \DateTime("+7 days")) {
+            return $expiresAt->format("Y-m-d H:i");
+        }
+
+        if ($expiresAt > new \DateTime("+1 day")) {
+            return " za " . $interval->format("%a dni");
+        }
+
+        return "za " . $interval->format("%h godz. %i min.");
+    }
+}
